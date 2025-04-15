dd7bed63d30c4063f127969aadf2a89277acaeab
DBT GISUL <lacchi.balaji@gisul.co.in>
2025-04-15 06:26:57 +0000

    commit

--- models/Snowflake/Departments.sql
+++ models/Snowflake/Departments.sql
@@ -1,4 +1,14 @@
-SELECT
-    CASE WHEN Department_ID = 10 THEN 1 ELSE Department_ID END AS Department_ID,
+-- File: models/Snowflake/Departments.sql
+
+{{ config(
+    post_hook=[
+      "DELETE FROM MYDB.POSTGRES_SNOWFLAKE_DB_MYSCHEMA.DEPARTMENTS WHERE Department_ID = 1",
+      "INSERT INTO MYDB.POSTGRES_SNOWFLAKE_DB_MYSCHEMA.DEPARTMENTS (Department_ID, Department_Name) VALUES (120, 'TestDept')"
+    ]
+) }}
+
+-- This is the main SELECT part for your model
+SELECT 
+    Department_ID, 
     Department_Name
 FROM MYDB.POSTGRES_SNOWFLAKE_DB_MYSCHEMA.DEPARTMENTS
